7bf832849360900bc2233fca0a1a274cdd740abf
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2022-11-25 22:25:11 +0300

    Add files via upload

---
+++ project/Partitioning.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Hyperlink_and_URL_partitioning
+{
+    partial class Partitioning
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(183, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите сслыку:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(14, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(475, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Формат URL - <протокол>://<хост>:<порт>/<URL‐путь>";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(123, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 27);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Разделить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, -1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Справка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label5.Location = new System.Drawing.Point(22, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Хост:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label4.Location = new System.Drawing.Point(22, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Протокол:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(123, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(363, 19);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(123, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(363, 19);
+            this.textBox3.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label6.Location = new System.Drawing.Point(22, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 22);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Порт:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label7.Location = new System.Drawing.Point(22, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 22);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "URL-путь:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(123, 223);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(363, 19);
+            this.textBox4.TabIndex = 24;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(123, 253);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(363, 65);
+            this.textBox5.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label3.Location = new System.Drawing.Point(256, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 22);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Порт необязательно";
+            // 
+            // Partitioning
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(505, 330);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Partitioning";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Partitioning";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Partitioning_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ project/Partitioning.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hyperlink_and_URL_partitioning
+{
+    public partial class Partitioning : Form
+    {
+        Autorization aut;
+        string name;
+
+        public Partitioning(Autorization aut, string name)
+        {
+            InitializeComponent();
+            this.aut = aut;
+            this.name = name;
+
+            using (StreamWriter writer = new StreamWriter("Log_" + name + ".log", File.Exists("Log_" + name + ".log")))
+            {
+                writer.WriteLine("User " + name + " logged in, at " + DateTime.Now.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            using (StreamWriter writer = new StreamWriter("Log_" + name + ".log", File.Exists("Log_" + name + ".log")))
+            {
+                writer.WriteLine($"The user has entered a link \"{a}\", at " + DateTime.Now.ToString());
+            }
+            try
+            {
+                System.Uri uri = new System.Uri(a);
+
+                // Получаем имя протокола
+                string protocol = uri.Scheme;
+
+                // Получаем имя хоста
+                string host = uri.Host;
+
+                // Получаем порт
+                int port = uri.Port;
+
+                // Получаем чистый URL 
+                string path = uri.LocalPath;
+
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+
+                if (uri.Port == -1 && !uri.IsFile)
+                {
+                    textBox2.Text = $"Неизвестный протокол ({uri.Scheme})";
+                }
+                else
+                    textBox2.Text = protocol;
+                
+                if (uri.IsFile)
+                {
+                    textBox3.Text = "Это файл. У файла нет хоста.";
+                    textBox4.Text = "Это файл. У файла нет порта.";
+                }
+                else if (port == -1)
+                {
+                    textBox3.Text = "Хост неизвестен. (Неверно указан адрес?)";
+                    textBox4.Text = "Порт неизвестен.";
+                }
+                else
+                {
+                    textBox3.Text = host;
+                    textBox4.Text = port.ToString();
+                }
+                textBox5.Text = path;
+            }
+            catch (UriFormatException)
+            {
+                MessageBox.Show("Адрес неверен", "Ошибка");
+                using (StreamWriter writer = new StreamWriter("Log_" + name + ".log", File.Exists("Log_" + name + ".log")))
+                {
+                    writer.WriteLine("User received an error - Wrong address, at " + DateTime.Now.ToString());
+                }
+            }
+        }
+
+        
+        private void Partitioning_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            aut.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                if (File.Exists("Справка.info")) Process.Start("Справка.info");
+                else
+                {
+                    MessageBox.Show("Файл со справкой отсутствует", "Ошибка");
+                    return;
+                }
+        }
+    }
+}
